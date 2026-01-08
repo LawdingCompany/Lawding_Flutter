4145ca3c1c3da1bd130ef6cc68e3d88032d291d7
KimRoks <kimroks3161@gmail.com>
2026-01-08 13:37:27 +0900

    chore: webview_flutter 의존성 추가

--- lawding_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ lawding_flutter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,9 +8,11 @@ import Foundation
 import firebase_analytics
 import firebase_core
 import firebase_crashlytics
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
