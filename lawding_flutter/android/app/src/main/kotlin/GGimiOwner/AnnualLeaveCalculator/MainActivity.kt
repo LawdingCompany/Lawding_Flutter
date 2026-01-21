e0690f9d214140cb301ba24b8b35e421daf0abc8
KimRoks <kimroks3161@gmail.com>
2026-01-22 02:50:39 +0900

    feat: 태블릿 가로모드 지원 (네이티브 리소스 분기)
    - values/bools.xml: is_tablet=false (휴대폰)
    - values-sw600dp/bools.xml: is_tablet=true (태블릿, 600dp 이상)
    - MainActivity.kt: 리소스 값에 따라 화면 방향 동적 설정
    - 휴대폰: 세로모드만, 태블릿: 모든 방향 허용

--- lawding_flutter/android/app/src/main/kotlin/GGimiOwner/AnnualLeaveCalculator/MainActivity.kt
+++ lawding_flutter/android/app/src/main/kotlin/GGimiOwner/AnnualLeaveCalculator/MainActivity.kt
@@ -1,5 +1,18 @@
 package GGimiOwner.AnnualLeaveCalculator
 
+import android.content.pm.ActivityInfo
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val isTablet = resources.getBoolean(R.bool.is_tablet)
+        requestedOrientation = if (isTablet) {
+            ActivityInfo.SCREEN_ORIENTATION_FULL_USER
+        } else {
+            ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+    }
+}
